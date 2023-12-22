1afff0d085c51f0d7be23a44d81960f668cc68b3
x4kUHD <x4kuhd.kim@gmail.com>
2023-12-21 20:38:13 -0800

    12/21/2023 navbar and main text

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,35 @@
-import "./index.css";
+import styles from "./main.module.css";
 import React from "react";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <div className="App">
-      <div>hello world</div>
+    <div className={`${styles.main}`}>
+      <NavBar className={styles.navbar}></NavBar>
+
+      <div className={styles.margin1}>
+        <div className={`${styles.center} ${styles.box1}`}>
+          <div className={styles.t1}>Hey, it's Eric 👋</div>
+          <div className={styles.t2}>
+            studying cs + interaction design @
+            <span className={styles.gt}> georgia tech</span>
+          </div>
+          <div className={styles.t3}>
+            currently a <span style={{ color: "black" }}> ux intern </span> @{" "}
+            <span style={{ color: "#00C2FF", textDecoration: "underline" }}>
+              {" "}
+              beaches app, inc.
+            </span>{" "}
+            previously an associate
+            <span style={{ color: "black" }}> product designer</span> @{" "}
+            <span style={{ color: "#0057FF", textDecoration: "underline" }}>
+              stord.
+            </span>
+            <br /> as an artist and ui/ux enthusiast, i am driven by a deep
+            appreciation for the fusion of art and technology.
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ my-app/src/components/NavBar.js
@@ -0,0 +1,22 @@
+import React from "react";
+import styles from "./navbar.module.css";
+
+function NavBar() {
+  return (
+    <nav className={styles.navbar}>
+      <a href="" className={styles.logo}>
+        💻 Eric
+      </a>
+      <ul>
+        <li>
+          <a href="">work</a>
+        </li>
+        <li>
+          <a href="">resume</a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
+export default NavBar;
