fbf8d0d5efb4d1c689c5c5ddca8dec817a1f3ed5
x4kUHD <x4kuhd.kim@gmail.com>
2023-12-25 02:49:34 -0800

    12/25/2023 random

---
+++ my-app/src/components/Calendar.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Calendar() {
+  return (
+    <div>
+      <div>
+        <iframe
+          style={{
+            border: "1px solid rgba(0, 0, 0, 0.1)",
+            width: "800px",
+            height: "450px",
+          }}
+          src="https://www.figma.com/embed?embed_host=share&url=https%3A%2F%2Fwww.figma.com%2Fproto%2FbUDVhjtKruQzhKJchU4gii%2Fapple-calendar%3Fpage-id%3D0%253A1%26node-id%3D48-91%26starting-point-node-id%3D1%253A8%26scaling%3Dmin-zoom%26mode%3Ddesign%26t%3D1xyfsfD2H8NlB9YW-1"
+          allowFullScreen
+        ></iframe>
+      </div>
+    </div>
+  );
+}
+
+export default Calendar;
